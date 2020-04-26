29fb2b0b261b57b01601f9f555effd6d785c6613
frankofilok <frankofilok@Frankofil.myslek.net>
2020-04-26 14:49:49 +0200

    clean code

--- settings.py
+++ settings.py
@@ -1,19 +1,22 @@
 #colors
-__color__ = [red, green, blue, yellow, black, white, gray] = [(255, 0, 0), (97, 204, 61), (71, 159, 214), (255, 255, 0), (0, 0, 0), (227, 209, 209), (60, 60, 60)]
+GREEN = (97, 204, 61)
+BLUE = (71, 159, 214)
+WHITE = (227, 209, 209)
+GRAY = (55, 55, 55)
 
 #screen
 scr_size = (width, height) = (600, 400)
 FPS = 50
-bgColor = gray
-colorChange = 3 #in sec
+bgColor = GRAY
+colorChange = 4 #in sec
 
 #Paddle data
 class PaddleData:
     sizeX = 80
     sizeY = 13
-    x = (width-sizeX)/2
-    y = height-height/8
-    color = blue
+    x = int((width-sizeX)/2)
+    y = int(height-height/8)
+    color = BLUE
     speed = 5
 
 #Brick data
@@ -24,11 +27,11 @@ class BrickData:
     topMarginLayer = 3
     sizeX = width/nBricks
     sizeY = height/3/columns - margin
-    color = green
+    color = GREEN
 
 #Ball data
 class BallData:
     radius = 8
     center = [int((width-radius)/2), int(2*height/4)]
-    color = white
+    color = WHITE
     speed = PaddleData.speed - 2
